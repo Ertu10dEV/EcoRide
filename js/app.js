915069ffe0d14b16ecdddb6b2513c53cc41fd78c
Ertu10dEV <vural.ertu@outlook.fr>
2025-06-30 23:59:43 +0200

    feat: première maquette page d'accueil (HTML/CSS/JS)

---
+++ js/app.js
@@ -0,0 +1,5 @@
+// Simple placeholder pour plus tard
+document.querySelector('.search-form').addEventListener('submit', e => {
+  e.preventDefault();
+  alert('La recherche de trajets arrivera bientôt !');
+});
